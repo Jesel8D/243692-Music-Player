caa46e488c117a96ff6a47cf0624c029e3a14d54
Jesel8 <jeselmozui@gmail.com>
2025-10-28 22:07:35 -0600

    Subida de certificados para debug

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  API_URL: 'https://api.spotify.com/v1',
+  AUTH_API_URL: 'https://accounts.spotify.com/api/token',
+  CLIENT_ID: '00975132502544089975cd85be31c94e',
+  CLIENT_SECRET: 'db36f555567e457db27f1e32bcea6a50',
+};
